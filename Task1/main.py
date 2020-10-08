9f2bc2888a587dbcaa4cb5a0428388b3965ebae6
Karol Podlewski <podlewski@hotmail.com>
2020-10-08 23:40:09 +0200

    feat: Added borough complaint types to script
    It is like 1st part of task is finished, but it easily could be polished.

--- Task1/main.py
+++ Task1/main.py
@@ -7,30 +7,35 @@ filePath = '311_Service_Requests_from_2010_to_Present.csv'
 
 current_time = datetime.now().strftime("%H:%M:%S")
 print(f'Current Time: {current_time}')
-start = timer()
+sTimer = timer()
 
-df = pd.read_csv(filePath, usecols=['Agency', 'Complaint Type', 'City'])
+df = pd.read_csv(filePath, usecols=['Agency', 'Complaint Type', 'Borough'])
+lTimer = timer()
 
-middle = timer()
-print(f'\nLoading time:  {round((middle - start), 5)} s')
+print('\n\nANALYSIS TIME:\n')
+print(f'Loading time:  {round((lTimer - sTimer), 5)} s')
 
 complaintType = df['Complaint Type'].value_counts()[:1].index.tolist()[0]
-
-citiesComlaintTypes = []
-cities = df['City'].dropna().unique()
-# for city in cities:
-#     cityDF = df[df['City'].str.contains(city, na=False)]
-#     citiesComlaintTypes.append((city, cityDF['Complaint Type'].value_counts()[:1].index.tolist()[0]))
+cTimer = timer()
 
 agency = df['Agency'].value_counts()[:1].index.tolist()[0]
+aTimer = timer()
 
-end = timer()
-print(f'Analysis time: {round((end - middle), 5)} s\n')
+boroughsComlaintTypes = []
+boroughs = df['Borough'].dropna().unique()
+for borough in boroughs:
+    reducedDF = df[df['Borough'].str.contains(borough, na=False)]
+    boroughsComlaintTypes.append((borough, reducedDF['Complaint Type'].value_counts()[:1].index.tolist()[0]))
+bTimer = timer()
 
-print(f'Complaint Type: {complaintType}')
-# print('Cities:')
-# for city, cityComplaintType in citiesComlaintTypes:
-#     print(f'  {city}: {cityComplaintType}')
-print(f'Agency: {agency}')
+print(f'Complaint Type: {round((cTimer - lTimer), 5)} s')
+print(f'Boroughs:  {round((bTimer - aTimer), 5)} s')
+print(f'Agency:    {round((aTimer - cTimer), 5)} s')
 
+print('\n\nANALYSIS RESULTS:\n')
 
+print(f'Complaint type: {complaintType}')
+print('Boroughs complaints types:')
+for borough, boroughComplaint in boroughsComlaintTypes:
+    print(f'   {borough}: {boroughComplaint}')
+print(f'Agency: {agency}\n\n')
