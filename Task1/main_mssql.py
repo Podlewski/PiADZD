6db4a922536b16c99322661534a769b88f39ea5e
shreq <shrek.kresh@o2.pl>
2020-10-18 02:12:24 +0200

    prepare dataset in script

--- Task1/main_mssql.py
+++ Task1/main_mssql.py
@@ -2,9 +2,12 @@ import sys
 from datetime import datetime
 from timeit import default_timer as timer
 import pyodbc
+import pandas as pd
 
 print(f'Current Time:\t{datetime.now().strftime("%H:%M:%S")}')
 sTimer = timer()
+df = pd.read_csv('V:/311_Service_Requests_from_2010_to_Present.csv', usecols=['Agency Name', 'Complaint Type', 'Borough'])
+df.to_csv('V:/311_Service_Requests_from_2010_to_Present-cut2.csv', sep='~')
 
 instance_name = sys.argv[1]
 connection = pyodbc.connect('Driver={SQL Server};'
@@ -39,14 +42,14 @@ cursor.execute("""bulk insert ServiceRequests..sr
                         firstrow = 2)""")
 lTimer = timer()
 
-cursor.execute("""select top 1 complaintType
+cursor.execute("""select top 10 complaintType
                   from ServiceRequests..sr
                   group by complaintType
                   order by count(*) desc""")
 cTimer = timer()
 cRows = cursor.fetchall()
 
-cursor.execute("""select top 1 agency
+cursor.execute("""select top 10 agency
                   from ServiceRequests..sr
                   group by agency
                   order by count(*) desc""")
@@ -59,7 +62,7 @@ cursor.execute("""select b.borough, b.complaintType
                         from (select complaintType, borough, count(*) as complaintCount
                               from ServiceRequests..sr
                               group by borough, complaintType) as c) as b
-                  where row_num = 1""")
+                  where row_num <= 10""")
 bTimer = timer()
 bRows = cursor.fetchall()
 cursor.close()
@@ -77,7 +80,7 @@ for row in cRows:
     print(f'\t{row[0]}')
 print('Complaints type by Borough:')
 for row in bRows:
-    print(f'\t{row[0]:<15}:{row[1]:>20}')
+    print(f'\t{row[0]:<15}: {row[1]}')
 print('Agency:')
 for row in aRows:
     print(f'\t{row[0]}')
