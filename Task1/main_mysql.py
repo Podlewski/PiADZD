99cee7f10b8d3bdc7ff1c45f5bb5905172fa3f53
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2020-10-20 00:14:59 +0200

    Added indexing to MySQL variant

--- Task1/main_mysql.py
+++ Task1/main_mysql.py
@@ -1,17 +1,7 @@
 import mysql.connector
 from timeit import default_timer as timer
 
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="test",
-    allow_local_infile=True
-)
-
-cursor = connection.cursor()
-sTimer = timer()
-
-query = "DROP DATABASE IF EXISTS ServiceRequests; \
+setupQuery = "DROP DATABASE IF EXISTS ServiceRequests; \
                 CREATE DATABASE ServiceRequests; \
                 USE ServiceRequests; \
                 CREATE TABLE sr(\
@@ -32,37 +22,73 @@ query = "DROP DATABASE IF EXISTS ServiceRequests; \
                 @col, @col, @borough) \
                 SET id=@id, agencyName=@agencyName, complaintType=@complaintType, borough=@borough;"
 
-for data in cursor.execute(query, multi=True):
-    if data.with_rows:
-        data = cursor.fetchall()
-
-lTimer = timer()
-
-cursor.execute("SELECT complaintType \
+complaintTypeQuery = "SELECT complaintType \
                 FROM ServiceRequests.sr \
                 GROUP BY complaintType \
                 ORDER BY COUNT(*) DESC \
-                LIMIT 10;")
-cTimer = timer()
-cRows = cursor.fetchall()
+                LIMIT 10;"
 
-cursor.execute("SELECT agencyName \
+agencyNameQuery = "SELECT agencyName \
                 FROM ServiceRequests.sr \
                 GROUP BY agencyName \
                 ORDER BY COUNT(*) DESC \
-                LIMIT 10;")
-aTimer = timer()
-aRows = cursor.fetchall()
+                LIMIT 10;"
 
-cursor.execute("SELECT b.borough, b.complaintType \
+boroughQuery = "SELECT b.borough, b.complaintType \
                 FROM (SELECT c.borough, c.complaintType, \
                 ROW_NUMBER() OVER(PARTITION BY c.borough ORDER BY c.complaintCount DESC) AS row_num \
                 FROM (SELECT complaintType, borough, COUNT(*) AS complaintCount \
                 FROM ServiceRequests.sr \
                 GROUP BY borough, complaintType) AS c) AS b \
-                WHERE row_num <= 10;")
+                WHERE row_num <= 10;"
+
+complaintTypeIndex = "CREATE INDEX complaintTypeIndex ON ServiceRequests.sr (complaintType);"
+agencyNameIndex = "CREATE INDEX agencyNameIndex ON ServiceRequests.sr (agencyName);"
+boroughIndex = "CREATE INDEX complaintTypeBoroughIndex ON ServiceRequests.sr (complaintType, borough);"
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="test",
+    allow_local_infile=True
+)
+
+cursor = connection.cursor()
+sTimer = timer()
+
+# for data in cursor.execute(setupQuery, multi=True):
+#     if data.with_rows:
+#         data = cursor.fetchall()
+lTimer = timer()
+
+cursor.execute(complaintTypeQuery)
+cTimer = timer()
+cRows = cursor.fetchall()
+
+cursor.execute(agencyNameQuery)
+aTimer = timer()
+aRows = cursor.fetchall()
+
+cursor.execute(boroughQuery)
 bTimer = timer()
 bRows = cursor.fetchall()
+
+cursor.execute(complaintTypeIndex)
+cursor.execute(agencyNameIndex)
+cursor.execute(boroughIndex)
+iTimer = timer()
+
+cursor.execute(complaintTypeQuery)
+cIndTimer = timer()
+cIndRows = cursor.fetchall()
+
+cursor.execute(agencyNameQuery)
+aIndTimer = timer()
+aIndRows = cursor.fetchall()
+
+cursor.execute(boroughQuery)
+bIndTimer = timer()
+bIndRows = cursor.fetchall()
 
 cursor.close()
 connection.close()
@@ -73,6 +99,13 @@ print('\nANALYSIS TIMES -------------------------')
 print(f'Complaint Type:{cTimer - lTimer:10.3f} s')
 print(f'Boroughs:      {bTimer - aTimer:10.3f} s')
 print(f'Agency:        {aTimer - cTimer:10.3f} s')
+
+print(f'\nIndexing time:     {iTimer - bTimer:7.2f} s')
+
+print('\nANALYSIS AFTER INDEXING TIMES -------------------------')
+print(f'Complaint Type:{cIndTimer - iTimer:10.3f} s')
+print(f'Boroughs:      {bIndTimer - aIndTimer:10.3f} s')
+print(f'Agency:        {aIndTimer - cIndTimer:10.3f} s')
 
 print('\nANALYSIS RESULTS -----------------------')
 print('Complaint type:')
