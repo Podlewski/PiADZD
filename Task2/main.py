fccff436825ad69af3ad438a30cbe27e8c4a5f0a
Karol Podlewski <podlewski@hotmail.com>
2020-11-03 11:24:55 +0100

    feat: Task 2

--- Task2/main.py
+++ Task2/main.py
@@ -1,13 +1,45 @@
+import itertools as it
 from pyspark import SparkContext
+import sys
+
+
+def split_line(line):
+    user, friends = line.split('\t')
+    return (user, friends.split(','))
+
+def get_mutal_friends(lines):
+    friends = lines.flatMap(lambda line: [((line[0], friend), float('inf')) for friend in line[1]])
+    possible_friends = lines.flatMap(lambda line: [(pair, 1) for pair in it.permutations(line[1], 2)])
+
+    pairs = friends.union(possible_friends)
+    pairs = pairs.reduceByKey(lambda x, y: x + y) \
+                 .filter(lambda val: val[1] != float('inf'))
+    return pairs
+
+def transform_pair(line):
+    return (line[0][0], (line[0][1], line[1]))
+
+def get_top_ten_recomendations(line):
+    return sorted(line, key = lambda x: (-x[1], x[0]))[:10]
+
+def parse_result(line):
+    user, recs = line[0], line[1]
+    rec_list = [str(rec[0]) for rec in recs]
+    return f'{user}\t{",".join(rec_list)}' 
+
 
 sc = SparkContext()
+file = sc.textFile(sys.argv[1])
 
-data = sc.textFile('2.txt').map(lambda row: row.split())
+lines = file.map(lambda line: split_line(line))
+pairs = get_mutal_friends(lines)
+result = pairs.map(transform_pair)\
+              .groupByKey() \
+              .mapValues(get_top_ten_recomendations) \
+              .sortByKey()
+parsed_resultd = result.map(parse_result).collect()
 
-withFriends = data.filter(lambda x: len(x) > 1).map(
-    lambda x: (x[0], x[1].split(",")))
-print(withFriends.take(5))
+with open('result.txt', 'w') as outfile:
+    outfile.write('\n'.join(parsed_resultd))
 
-withoutFriends = data.filter(lambda x: len(
-    x) == 1).flatMap(lambda x: x)
-print(withoutFriends.take(5))
+sc.stop()
