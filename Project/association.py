5ec5efe4dbbbb4428114820df70c051663411342
shreq <shrek.kresh@o2.pl>
2021-01-16 18:27:46 +0100

    fp growth

--- Project/association.py
+++ Project/association.py
@@ -2,13 +2,11 @@ from os import system, name
 
 import pyspark
 from pyspark.ml import Pipeline
-from pyspark.ml.linalg import DenseVector
-from pyspark.sql import SparkSession, types
-from pyspark.ml.feature import VectorAssembler, StringIndexer
-from pyspark.ml.fpm import FPGrowth
-from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType
+from pyspark.ml.feature import StringIndexer
+import pyspark.ml.fpm as mlfpm
+import pyspark.mllib.fpm as mllibfpm
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import array, sum
 
 
 def clear_output(lame: bool = False):
@@ -18,34 +16,24 @@ def clear_output(lame: bool = False):
         system('cls' if name == 'nt' else 'clear')
 
 
-def split_line(line):
-    items = line.split(',')
-    return items[:1], (items[1:])
-
-
 ss = SparkSession.builder.getOrCreate()
-clear_output(True)
-
-data = ss.read.csv('data/dfny.csv', header=True, inferSchema=True)
+data = ss.read.csv('data/dfch.csv', header=True, inferSchema=True)
 df = data.toDF(*data.columns)
 clear_output(True)
 
 
-indexers = [StringIndexer(inputCol=col, outputCol=col + '_idx').fit(df)
-            for col in list(set(df.columns) - {'Date'})]
-            # for col in df.columns]
-dfi = Pipeline(stages=indexers).fit(df).transform(df)
-dfi = dfi.select([col for col in dfi.columns if '_idx' in col])
-dff = VectorAssembler(inputCols=dfi.columns, outputCol='features').transform(dfi)
+# indexers = [StringIndexer(inputCol=col, outputCol=col + '_idx').fit(df)
+#             for col in list(set(df.columns) - {'Date'})]
+#             # for col in df.columns]
+# dfi = Pipeline(stages=indexers).fit(df).transform(df)
+# dfi = dfi.select([col for col in dfi.columns if '_idx' in col])
+# dff = dfi.withColumn('features', array(*dfi.columns))
 
-aa = udf(lambda x: x, types.ArrayType(types.DoubleType()))
-dff = dff.withColumn('features_', aa(dff.features))
+fpm = mlfpm.FPGrowth(itemsCol='features', minSupport=0.1, minConfidence=0.1)
+model = fpm.fit(df.withColumn('features', array(*list(set(df.columns) - {'Crime Details', 'Crime Description'}))))
+result = model.freqItemsets.collect()
+for r in result:
+    if len(r['items']) > 1:
+        print(r)
 
-dff.show(5)
-quit()
-
-[print(x) for x in dff.select('features').take(20)]
-
-result = FPGrowth(itemsCol='features', minSupport=0.2, minConfidence=0.5).fit(dff)
-
-# print(result.take(5))
+# fpm = mllibfpm.FPGrowth.train(df.rdd.map(list).cache(), minSupport=0.2)
