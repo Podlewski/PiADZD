9dbafe04b8a0bc9e38dc28673ba04454b546154f
shreq <shrek.kresh@o2.pl>
2021-01-17 02:23:49 +0100

    fixed output

--- Project/association.py
+++ Project/association.py
@@ -5,7 +5,9 @@ from typing import List
 
 from pyspark.ml.fpm import FPGrowth
 from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.functions import array
+import pyspark.sql.functions as funcs
 from pyspark.sql.types import Row, StringType
 from pyspark.sql.udf import UserDefinedFunction
 
@@ -17,16 +19,29 @@ def clear_output() -> None:
         system('cls' if name == 'nt' else 'clear')
 
 
-def print_result(result: List[Row]) -> None:
+def print_result(result: List[Row], source_shape: (int, int)) -> None:
     max_tuple_size = max([len(r['items']) for r in result])
     for r in result:
         if len(r['items']) > 1 or max_tuple_size < 2:
-            print(r)
+            support = r['freq'] / source_shape[0]
+            print(f'{list_to_string(r["items"])}; supp={support}')
+
+
+def list_to_string(list_: list) -> str:
+    return f'[{", ".join(x for x in list_)}]'
+
+
+def get_shape(df: DataFrame) -> (int, int):
+    return df.count(), len(df.columns)
+
+
+def contains(x, y):
+    return y if len(set(x) - set(y)) == 0 else None
 
 
 # SETUP #######################################################################
 ss = SparkSession.builder.getOrCreate()
-fpm = FPGrowth(itemsCol='features', minSupport=0.2, minConfidence=0.1)
+fpm = FPGrowth(itemsCol='features', minSupport=0.1, minConfidence=0.5)
 a__prefix = UserDefinedFunction(lambda x: 'a_' + x, StringType())
 s__prefix = UserDefinedFunction(lambda x: 's_' + x, StringType())
 r__prefix = UserDefinedFunction(lambda x: 'r_' + x, StringType())
@@ -34,7 +49,7 @@ v_prefix = UserDefinedFunction(lambda x: 'v' + x, StringType())
 s_prefix = UserDefinedFunction(lambda x: 's' + x, StringType())
 
 # NEW YORK ####################################################################
-data = ss.read.csv('data/dfny.csv', header=True, inferSchema=True).drop('_c0')
+data = ss.read.csv('data/dfny.csv', header=True, inferSchema=True)
 dfny = data.toDF(*data.columns)
 del data
 for col in ['Suspect Age Group', 'Victim Age Group']:
@@ -52,7 +67,7 @@ dfny = dfny.withColumn(
 ).select('features')
 
 # CHICAGO #####################################################################
-data = ss.read.csv('data/dfch.csv', header=True, inferSchema=True).drop('_c0')
+data = ss.read.csv('data/dfch.csv', header=True, inferSchema=True)
 dfch = data.toDF(*data.columns)
 del data
 dfch = dfch.withColumn(
@@ -60,7 +75,7 @@ dfch = dfch.withColumn(
 ).select('features')
 
 # LOS ANGELES #################################################################
-data = ss.read.csv('data/dfla.csv', header=True, inferSchema=True).drop('_c0')
+data = ss.read.csv('data/dfla.csv', header=True, inferSchema=True)
 dfla = data.toDF(*data.columns)
 del data
 dfla = dfla.withColumn('Victim Age Group', a__prefix(dfla['Victim Age Group']))
@@ -71,11 +86,11 @@ dfla = dfla.withColumn(
 ).select('features')
 
 # CALCULATIONS ################################################################
-resny = fpm.fit(dfny).freqItemsets.collect()
+resny = fpm.fit(dfny)
 del dfny
-resch = fpm.fit(dfch).freqItemsets.collect()
+resch = fpm.fit(dfch)
 del dfch
-resla = fpm.fit(dfla).freqItemsets.collect()
+resla = fpm.fit(dfla)
 del dfla
 
 # OUTPUT ######################################################################
@@ -83,15 +98,21 @@ clear_output()
 with open('result_FPGrowth.txt', 'w') as file:
     with redirect_stdout(file):
         print('\n###### NEW YORK #####')
-        print_result(resny)
+        resny.associationRules.show(resny.associationRules.count(), False)
+        resny.freqItemsets.show(resny.freqItemsets.count(), False)
         print('\n###### CHICAGO ######')
-        print_result(resch)
+        resch.associationRules.show(resch.associationRules.count(), False)
+        resch.freqItemsets.show(resch.freqItemsets.count(), False)
         print('\n#### LOS ANGELES ####')
-        print_result(resla)
+        resla.associationRules.show(resla.associationRules.count(), False)
+        resla.freqItemsets.show(resla.freqItemsets.count(), False)
 
 print('\n###### NEW YORK #####')
-print_result(resny)
+resny.associationRules.show(resny.associationRules.count(), False)
+resny.freqItemsets.show(resny.freqItemsets.count(), False)
 print('\n###### CHICAGO ######')
-print_result(resch)
+resch.associationRules.show(resch.associationRules.count(), False)
+resch.freqItemsets.show(resch.freqItemsets.count(), False)
 print('\n#### LOS ANGELES ####')
-print_result(resla)
+resla.associationRules.show(resla.associationRules.count(), False)
+resla.freqItemsets.show(resla.freqItemsets.count(), False)
